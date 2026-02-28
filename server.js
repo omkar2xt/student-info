dcf5db343e01a28af996935e3a91851c7795c2fd
Omkar Gurav <omkargurav@studentsportal.local>
2026-02-28 14:52:06 +0530

    Fix root route for Render

--- server.js
+++ server.js
@@ -11,6 +11,11 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static(path.join(__dirname)));
 
+// Serve the student portal on the root route
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'student-portal.html'));
+});
+
 const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
 });
